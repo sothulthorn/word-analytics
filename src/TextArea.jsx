3b51131b6f3d6ea0a058718ef4249cd788b6bf49
sothulthorn <thornsothul@gmail.com>
2024-07-08 20:52:22 +0900

    Implement Text Validation

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,5 +1,38 @@
+import { useState } from 'react';
+import Warning from './Warning';
+
 const TextArea = () => {
-  return <textarea className="textarea" />;
+  const [text, setText] = useState('');
+  const [warningText, setWarningText] = useState('');
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+
+    if (newText.includes('<script>')) {
+      setWarningText('No script tag allowed!');
+      newText = newText.replace('<script>', '');
+    } else if (newText.includes('@')) {
+      setWarningText('No @ symbol allowed!');
+      newText = newText.replace('@', '');
+    } else {
+      setWarningText('');
+    }
+
+    setText(newText);
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        value={text}
+        placeholder="Enter your text"
+        spellCheck="false"
+        onChange={handleChange}
+      />
+
+      <Warning warningText={warningText} />
+    </div>
+  );
 };
 
 export default TextArea;
